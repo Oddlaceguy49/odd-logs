29ace08f9dbfde66654549cb152cc5b872dd5aa3
Oddlaceguy49 <116045403+Oddlaceguy49@users.noreply.github.com>
2025-08-08 13:36:53 -0700

    feat: add toggle animations, system toggle icon, and removed sidebar
    Also improved the organization of the tags slightly. Layouts are now in there correct places. I think it looks pretty clean. Onward!

--- src/components/ui/ModeToggle.svelte
+++ src/components/ui/ModeToggle.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+    import { userPrefersMode, setMode, resetMode } from "mode-watcher";
     import SunIcon from "@lucide/svelte/icons/sun";
     import MoonIcon from "@lucide/svelte/icons/moon";
+    import LaptopMinimalIcon from "@lucide/svelte/icons/laptop-minimal";
 
-    import { resetMode, setMode } from "mode-watcher";
     import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
     import { buttonVariants } from "$lib/components/ui/button/index.js";
 </script>
@@ -12,14 +13,30 @@
         class={buttonVariants({ variant: "outline", size: "icon" })}
     >
         <SunIcon
-            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
+            class="h-[1.2rem] w-[1.2rem] !transition-all {userPrefersMode.current ===
+            'light'
+                ? 'rotate-0 scale-100'
+                : 'rotate-90 scale-0'}"
         />
+
         <MoonIcon
-            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
+            class="absolute h-[1.2rem] w-[1.2rem] !transition-all {userPrefersMode.current ===
+            'dark'
+                ? 'rotate-0 scale-100'
+                : '-rotate-90 scale-0'}"
         />
+
+        <LaptopMinimalIcon
+            class="absolute h-[1.2rem] w-[1.2rem] !transition-all {userPrefersMode.current ===
+            'system'
+                ? 'rotate-0 scale-100'
+                : '-rotate-90 scale-0'}"
+        />
+
         <span class="sr-only">Toggle theme</span>
     </DropdownMenu.Trigger>
     <DropdownMenu.Content align="end">
+        <!-- 3. Use Svelte's on:click for event handling -->
         <DropdownMenu.Item onclick={() => setMode("light")}
             >Light</DropdownMenu.Item
         >
