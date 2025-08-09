d22c490114474ec3ee2671fc879a7ddf40efddaa
Oddlaceguy49 <116045403+Oddlaceguy49@users.noreply.github.com>
2025-08-09 13:01:22 -0700

    feat: add basic blog viewing system using slugs

---
+++ src/content.config.ts
@@ -0,0 +1,47 @@
+import { defineCollection, reference, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const logs = defineCollection({
+    loader: glob({
+        pattern: ["**/[^_]*.mdx", "*.md"],
+        base: "./src/data/logs",
+    }),
+    schema: z.object({
+        title: z.string(),
+        slug: z.string().optional(),
+        pubDate: z.date(),
+        updatedDate: z.coerce.date().optional(),
+        description: z.string(),
+        author: reference("authors"),
+        draft: z.boolean().optional(),
+        tags: z.array(z.string()),
+        category: z.string().optional(),
+        readingTime: z.number().optional(),
+        canonicalUrl: z.string().url().optional(),
+        image: z.object({
+            url: z.string(),
+            alt: z.string(),
+            width: z.number().optional(),
+            height: z.number().optional(),
+        }),
+        lang: z.string().optional(),
+        ogType: z.string().optional(),
+        featured: z.boolean().optional(),
+        series: z.string().optional(),
+    }),
+});
+
+const authors = defineCollection({
+    loader: glob({ pattern: "**/[^_]*.json", base: "./src/data/authors" }),
+    schema: z.object({
+        name: z.string(),
+        avatar: z.string().optional(),
+        bio: z.string().optional(),
+        contacts: z.array(z.string()).optional(),
+    }),
+});
+
+export const collections = {
+    logs,
+    authors,
+};
