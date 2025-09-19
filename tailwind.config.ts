c0fe84caa66db2425cf25ab56deeb6e578fca81d
Oddlaceguy49 <116045403+Oddlaceguy49@users.noreply.github.com>
2025-09-19 07:28:33 -0700

    chore: add tailwindcss-animate and update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,36 @@
+import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
+import animate from "tailwindcss-animate";
 
-export default {
+/** @type {import('tailwindcss').Config} */
+const config: Config = {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+
     darkMode: "class",
+
+    theme: {
+        extend: {
+            keyframes: {
+                fade: {
+                    from: { opacity: "0" },
+                    to: { opacity: "1" },
+                },
+                "slide-from-right": {
+                    from: { transform: "translateX(30px)" },
+                    to: { transform: "translateX(0)" },
+                },
+                "slide-from-left": {
+                    from: { transform: "translateX(-30px)" },
+                    to: { transform: "translateX(0)" },
+                },
+            },
+            animation: {
+                "fade-in": "fade 0.4s ease-out",
+                "slide-in-right": "slide-from-right 0.4s ease-out",
+            },
+        },
+    },
+
     plugins: [
         plugin(function ({ addUtilities }) {
             const utilities = {
@@ -15,5 +44,8 @@ export default {
             };
             addUtilities(utilities);
         }),
+        animate,
     ],
 };
+
+export default config;
